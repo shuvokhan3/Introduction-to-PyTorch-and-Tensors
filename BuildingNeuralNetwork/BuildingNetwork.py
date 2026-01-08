1609d68a3fcb03334ffea5b41eeaec537be45a0d
shuvokhan3 <shuvoj360@gmail.com>
2026-01-08 22:04:33 +0600

    Building network

--- BuildingNeuralNetwork/BuildingNetwork.py
+++ BuildingNeuralNetwork/BuildingNetwork.py
@@ -5,7 +5,6 @@ import torch.nn as nn
 # # Values are normalized (scaled between 0 and 1)
 # weather_data = torch.tensor([[0.5, 0.8, 0.2]])  # Hot, very humid, light wind
 weather_data = torch.tensor([1,2,3], dtype=torch.float32)
-
 #Create network
 layer = nn.Linear(in_features=3, out_features=2)
 
